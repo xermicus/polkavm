541ad51846e584d13eed26cff0c022c4586baf60
Jan Bujak <jan@parity.io>
2024-11-01 09:26:10 +0000

    Update `pallet-revive` tests

--- ci/jobs/build-and-test-pallet-revive.sh
+++ ci/jobs/build-and-test-pallet-revive.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
 
+rustup toolchain install --component=rust-src nightly-2024-11-01-x86_64-unknown-linux-gnu
+
 POLKAVM_CRATES_ROOT="$(pwd)/proxy-crates"
 
 cd ../..
@@ -14,18 +16,18 @@ if [ ! -d "polkadot-sdk" ]; then
     git clone --depth 1 "https://github.com/paritytech/polkadot-sdk.git"
 fi
 cd polkadot-sdk
-COMMIT=8c548eb6af3ac277575591a076f4e33b150bdc11
+COMMIT=2700dbf2dda8b7f593447c939e1a26dacdb8ce45
 git fetch --depth=1 origin $COMMIT
 git checkout $COMMIT
 
 echo '[toolchain]' > rust-toolchain.toml
-echo 'channel = "nightly-2024-07-10"' >> rust-toolchain.toml
+echo 'channel = "nightly-2024-11-01"' >> rust-toolchain.toml
 
-PALLET_REVIVE_FIXTURES_RUSTUP_TOOLCHAIN=riscv32em-nightly-2024-01-05-r0-x86_64-unknown-linux-gnu \
+PALLET_REVIVE_FIXTURES_RUSTUP_TOOLCHAIN=nightly-2024-11-01-x86_64-unknown-linux-gnu \
 PALLET_REVIVE_FIXTURES_STRIP=0 \
 PALLET_REVIVE_FIXTURES_OPTIMIZE=1 \
 cargo test \
-    --config "patch.crates-io.polkavm010.path='$POLKAVM_CRATES_ROOT/polkavm010'" --config "patch.crates-io.polkavm010.package='polkavm'" \
-    --config "patch.crates-io.polkavm-derive010.path='$POLKAVM_CRATES_ROOT/polkavm-derive010'" --config "patch.crates-io.polkavm-derive010.package='polkavm-derive'" \
-    --config "patch.crates-io.polkavm-linker010.path='$POLKAVM_CRATES_ROOT/polkavm-linker010'" --config "patch.crates-io.polkavm-linker010.package='polkavm-linker'" \
-    --features riscv -p pallet-revive
+    --config "patch.crates-io.polkavm013.path='$POLKAVM_CRATES_ROOT/polkavm013'" --config "patch.crates-io.polkavm013.package='polkavm'" \
+    --config "patch.crates-io.polkavm-derive014.path='$POLKAVM_CRATES_ROOT/polkavm-derive014'" --config "patch.crates-io.polkavm-derive014.package='polkavm-derive'" \
+    --config "patch.crates-io.polkavm-linker014.path='$POLKAVM_CRATES_ROOT/polkavm-linker014'" --config "patch.crates-io.polkavm-linker014.package='polkavm-linker'" \
+    -p pallet-revive
