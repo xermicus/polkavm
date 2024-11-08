9c2d23116505413dd9bab450c5e6f74ec4b318f8
Jan Bujak <jan@parity.io>
2024-11-08 10:58:42 +0000

    Remove `TOOLCHAIN_VERSION` from the build benchmarks script

--- guest-programs/build-benchmarks.sh
+++ guest-programs/build-benchmarks.sh
@@ -6,16 +6,14 @@ cd "${0%/*}/"
 
 source build-common.sh
 
-TOOLCHAIN_VERSION="1.75.0"
-
 BUILD_WASM=0
 BUILD_CKBVM=0
 BUILD_NATIVE_X86_64=0
 BUILD_NATIVE_X86=0
 
 if [ "${BUILD_BENCHMARKS_INSTALL_ALL_TOOLCHAINS:-}" == "1" ]; then
-    rustup run $TOOLCHAIN_VERSION rustup target add wasm32-unknown-unknown
-    rustup run $TOOLCHAIN_VERSION rustup target add riscv64imac-unknown-none-elf
+    rustup target add wasm32-unknown-unknown
+    rustup target add riscv64imac-unknown-none-elf
     if [ ! -d "/tmp/solana-platform-tools-1.39" ]; then
         echo "Downloading Solana platform tools..."
         curl -Lo /tmp/platform-tools-linux-x86_64-1.39.tar.bz2 'https://github.com/anza-xyz/platform-tools/releases/download/v1.39/platform-tools-linux-x86_64.tar.bz2'
@@ -26,32 +24,27 @@ if [ "${BUILD_BENCHMARKS_INSTALL_ALL_TOOLCHAINS:-}" == "1" ]; then
     export SOLANA_PLATFORM_TOOLS_DIR='/tmp/solana-platform-tools-1.39'
 fi
 
-if [[ "$(rustup toolchain list)" =~ "$TOOLCHAIN_VERSION" ]]; then
-    if [[ "$(rustup run $TOOLCHAIN_VERSION rustup target list --installed)" =~ "wasm32-unknown-unknown" ]]; then
-        BUILD_WASM=1
-    else
-        echo "WARN: the wasm32-unknown-unknown target is not installed; WASM binaries won't be built!"
-        echo "      You can add it with: rustup run $TOOLCHAIN_VERSION rustup target add wasm32-unknown-unknown"
-    fi
+if [[ "$(rustup target list --installed)" =~ "wasm32-unknown-unknown" ]]; then
+    BUILD_WASM=1
+else
+    echo "WARN: the wasm32-unknown-unknown target is not installed; WASM binaries won't be built!"
+    echo "      You can add it with: rustup target add wasm32-unknown-unknown"
+fi
 
-    if [[ "$(rustup run $TOOLCHAIN_VERSION rustup target list --installed)" =~ "riscv64imac-unknown-none-elf" ]]; then
-        BUILD_CKBVM=1
-    else
-        echo "WARN: the riscv64imac-unknown-none-elf target is not installed; CKBVM binaries won't be built!"
-        echo "      You can add it with: rustup run $TOOLCHAIN_VERSION rustup target add riscv64imac-unknown-none-elf"
-    fi
+if [[ "$(rustup target list --installed)" =~ "riscv64imac-unknown-none-elf" ]]; then
+    BUILD_CKBVM=1
+else
+    echo "WARN: the riscv64imac-unknown-none-elf target is not installed; CKBVM binaries won't be built!"
+    echo "      You can add it with: rustup target add riscv64imac-unknown-none-elf"
+fi
 
-    if [[ "$(rustup run $TOOLCHAIN_VERSION rustc --print cfg)" =~ "target_os=\"linux\"" ]]; then
-        if [[ "$(rustup run $TOOLCHAIN_VERSION rustc --print cfg)" =~ "target_arch=\"x86_64\"" ]]; then
-            BUILD_NATIVE_X86_64=1
-            if [[ "$(rustup run $TOOLCHAIN_VERSION rustup target list --installed)" =~ "i686-unknown-linux-gnu" ]]; then
-                BUILD_NATIVE_X86=1
-            fi
+if [[ "$(rustc --print cfg)" =~ "target_os=\"linux\"" ]]; then
+    if [[ "$(rustc --print cfg)" =~ "target_arch=\"x86_64\"" ]]; then
+        BUILD_NATIVE_X86_64=1
+        if [[ "$(rustup target list --installed)" =~ "i686-unknown-linux-gnu" ]]; then
+            BUILD_NATIVE_X86=1
         fi
     fi
-else
-    echo "WARN: Rust $TOOLCHAIN_VERSION is not installed; non-PolkaVM binaries won't be built!"
-    echo "      You can add it with: rustup toolchain install $TOOLCHAIN_VERSION"
 fi
 
 if [ "${SOLANA_PLATFORM_TOOLS_DIR:-}" == "" ]; then
@@ -108,22 +101,22 @@ function build_benchmark() {
 
     if [ "${BUILD_WASM}" == "1" ]; then
         echo "> Building: '$1' (wasm)"
-        RUSTFLAGS="-C target-cpu=mvp -C target-feature=-sign-ext $extra_flags" rustup run $TOOLCHAIN_VERSION cargo build -q --target=wasm32-unknown-unknown --release --bin $1 -p $1
+        RUSTFLAGS="-C target-cpu=mvp -C target-feature=-sign-ext $extra_flags" cargo build -q --target=wasm32-unknown-unknown --release --bin $1 -p $1
     fi
 
     if [ "${BUILD_NATIVE_X86_64}" == "1" ]; then
         echo "> Building: '$1' (native, x86_64)"
-        RUSTFLAGS="$extra_flags" rustup run $TOOLCHAIN_VERSION cargo build -q --target=x86_64-unknown-linux-gnu --release --lib -p $1
+        RUSTFLAGS="$extra_flags" cargo build -q --target=x86_64-unknown-linux-gnu --release --lib -p $1
     fi
 
     if [ "${BUILD_NATIVE_X86}" == "1" ]; then
         echo "> Building: '$1' (native, i686)"
-        RUSTFLAGS="$extra_flags" rustup run $TOOLCHAIN_VERSION cargo build -q --target=i686-unknown-linux-gnu --release --lib -p $1
+        RUSTFLAGS="$extra_flags" cargo build -q --target=i686-unknown-linux-gnu --release --lib -p $1
     fi
 
     if [ "${BUILD_CKBVM}" == "1" ]; then
         echo "> Building: '$1' (CKB VM)"
-        RUSTFLAGS="$extra_flags -C target-feature=+zba,+zbb,+zbc,+zbs -C link-arg=-s --cfg=target_ckb_vm" rustup run $TOOLCHAIN_VERSION cargo build -q --target=riscv64imac-unknown-none-elf --release --bin $1 -p $1
+        RUSTFLAGS="$extra_flags -C target-feature=+zba,+zbb,+zbc,+zbs -C link-arg=-s --cfg=target_ckb_vm" cargo build -q --target=riscv64imac-unknown-none-elf --release --bin $1 -p $1
     fi
 
     if [ "${SOLANA_PLATFORM_TOOLS_DIR:-}" != "" ]; then
