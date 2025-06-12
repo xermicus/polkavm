5afed163e1fa5693fb345a1d81a9a4fbee3f076b
Jan Bujak <jan@parity.io>
2025-06-12 06:19:48 +0000

    Fix fuzz test compilation

--- fuzz/fuzz_targets/fuzz_polkavm.rs
+++ fuzz/fuzz_targets/fuzz_polkavm.rs
@@ -513,7 +513,7 @@ fn build_program_blob(data: Vec<OperationKind>) -> ProgramBlob {
     let mut builder = ProgramBlobBuilder::new_64bit();
     builder.add_export_by_basic_block(0, b"main");
     builder.set_code(&code, &[]);
-    ProgramBlob::parse(builder.into_vec().into()).unwrap()
+    ProgramBlob::parse(builder.into_vec().unwrap().into()).unwrap()
 }
 
 fn interpreter_fuzzer_harness(data: Vec<OperationKind>) {
