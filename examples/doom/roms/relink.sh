61973c077784905f7bf91284565028715bc75b03
Jan Bujak <jan@parity.io>
2025-08-11 03:06:35 +0000

    Update DOOM's `relink.sh` to also build the 64-bit blob

--- examples/doom/roms/relink.sh
+++ examples/doom/roms/relink.sh
@@ -4,3 +4,6 @@ set -euo pipefail
 
 zstd -f -d -o /tmp/doom.elf ../../../test-data/doom_O3_dwarf5.elf.zst
 cargo run -p polkatool link -s /tmp/doom.elf -o doom.polkavm
+
+zstd -f -d -o /tmp/doom64.elf ../../../test-data/doom_64.elf.zst
+cargo run -p polkatool link -s /tmp/doom64.elf -o doom64.polkavm
